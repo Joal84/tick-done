74be39daf5556a10b631eaf903cb20b1713e597c
joal84 <joalmonog@gmail.com>
2023-04-21 21:09:18 +0200

    Environment variables created

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// const urlSupa = "https://pvwmmlshdbxidotgjrhd.supabase.co";
-// const keySupa =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2d21tbHNoZGJ4aWRvdGdqcmhkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3Mjc0OTQyMywiZXhwIjoxOTg4MzI1NDIzfQ.FAVAacBb6xZta4ttGtDrOqi5I5dYjk3l16RqT73-B5g";
+const keySupa = import.meta.env.VITE_KEY;
+const supaUrl = "https://pvwmmlshdbxidotgjrhd.supabase.co";
 
-const urlSupa = import.meta.env.VITE_URL;
-const keySupa = import.meta.env.VITE_API_KEY;
-
-export const supabase = createClient(urlSupa, keySupa);
+export const supabase = createClient(supaUrl, keySupa);
